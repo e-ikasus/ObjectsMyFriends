984a5e3d96d2ebf7030bfaf40db06a942ee1471d
e-ikasus <contact@e-ikasus.fr>
2023-05-01 18:49:23 +0200

    Activate search filters.

--- src/main/java/fr/eikasus/objectsmyfriends/controller/WelcomeServlet.java
+++ src/main/java/fr/eikasus/objectsmyfriends/controller/WelcomeServlet.java
@@ -1,9 +1,16 @@
 package fr.eikasus.objectsmyfriends.controller;
 
 import com.google.gson.Gson;
+import com.mysql.cj.protocol.a.NativeConstants;
+import com.sun.deploy.net.HttpRequest;
 import fr.eikasus.objectsmyfriends.model.bll.ManagerFactory;
+import fr.eikasus.objectsmyfriends.model.bo.Category;
 import fr.eikasus.objectsmyfriends.model.bo.Item;
+import fr.eikasus.objectsmyfriends.model.bo.User;
 import fr.eikasus.objectsmyfriends.model.misc.ModelException;
+import fr.eikasus.objectsmyfriends.model.misc.Search;
+import fr.eikasus.objectsmyfriends.model.misc.UserRole;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
 import javax.inject.Inject;
@@ -15,38 +22,120 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Base64;
-import java.util.HashMap;
+import java.util.*;
+import java.util.stream.Stream;
 
 @WebServlet(name = "WelcomeServlet", urlPatterns = {"/welcome", "/index.jsp", "/index.html"})
 public class WelcomeServlet extends HttpServlet
 {
+	private final String CATEGORY = "category";
+
+	private final String KEYWORDS = "keywords";
+
+	private final String OPENED_BIDS = "openedBids";
+
+	private final String CURRENT_BIDS = "currentBids";
+
+	private final String WON_BIDS = "wonBids";
+
+	private final String MY_CURRENT_SALES = "myCurrentSales";
+
+	private final String MY_PENDING_SALES = "myPendingSales";
+
+	private final String MY_ENDED_SALES = "myEndedSales";
+
+	private final String SEARCH_TYPE = "searchType";
+
+	private final String[] knownParams = {CATEGORY, KEYWORDS, OPENED_BIDS, CURRENT_BIDS, WON_BIDS, MY_CURRENT_SALES, MY_PENDING_SALES, MY_ENDED_SALES, SEARCH_TYPE};
+
 	@Inject ManagerFactory managerFactory;
 
-	@SuppressWarnings("unchecked")
 	@Override protected void doGet(@NotNull HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		Search searchOptions = new Search();
+		UserRole role;
+		String requestedCategory;
+		Category category;
+		List<Category> categories;
+
+		// Connected user.
+		User user = (User) request.getSession().getAttribute("user");
+
+		// No item is currently selected. This is necessary for the image handler.
+		request.getSession().removeAttribute("item");
+		request.getSession().removeAttribute("itemImages");
+
+		// Read parameters from request ok cookie.
+		readCriteriaSearch(request, response);
+
+		// create and define search criteria.
+		if (request.getAttribute(OPENED_BIDS) != null ) searchOptions.setOpenedBids();
+		if (request.getAttribute(CURRENT_BIDS) != null ) searchOptions.setMyCurrentBids();
+		if (request.getAttribute(WON_BIDS) != null ) searchOptions.setMyWonBids();
+		if (request.getAttribute(MY_CURRENT_SALES) != null ) searchOptions.setMyCurrentSales();
+		if (request.getAttribute(MY_PENDING_SALES) != null ) searchOptions.setMyWaitingSales();
+		if (request.getAttribute(MY_ENDED_SALES) != null ) searchOptions.setMyEndedSales();
+
+		// Define the user role.
+		if (searchOptions.isMyCurrentSales() || searchOptions.isMyWaitingSales() || searchOptions.isMyEndedSales() ) role = UserRole.SELLER;
+		else role = UserRole.BUYER;
+
+		try
+		{
+			// Read the available categories. Will also be used by the view.
+			categories = managerFactory.getCategoryManager().find(null);
+			request.setAttribute("categories", categories);
+
+			// Category chosen by the user.
+			requestedCategory = (String) request.getAttribute(CATEGORY);
+
+			if (requestedCategory == null) category = null;
+			else category = categories.stream().filter(c -> StringUtils.equalsIgnoreCase(c.getLabel(), requestedCategory)).findFirst().orElse(null);
+
+			request.setAttribute("items", managerFactory.getItemManager().findByCriteria(user, role, searchOptions, category, (String) request.getAttribute(KEYWORDS)));
+			//request.setAttribute("items", managerFactory.getItemManager().find(null));
+		}
+		catch (ModelException e)
+		{
+			request.setAttribute("items", new ArrayList<Item>());
+		}
+
+		// Go to the welcome page => take search options into account.
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/welcome.jsp");
+		requestDispatcher.forward(request, response);
+	}
+
+	@Override protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		doGet(request, response);
+	}
+
+	/* ************** */
+	/* Helper methods */
+	/* ************** */
+
+	/**
+	 * Read filter options parameters from the request.
+	 * <p>
+	 * This method read parameters from the request and update those that were
+	 * previously supplied and stored in the cookie named "searchOptions2".
+	 * In all cases, the parameters used to filter the items shown in the welcome
+	 * page are to be taken from the request after the execution of this method.
+	 *
+	 * @param request Http request.
+	 * @param response Http response.
+	 */
+
+	@SuppressWarnings("unchecked")
+	private void readCriteriaSearch(HttpServletRequest request, HttpServletResponse response)
 	{
 		HashMap<String, String> savedParams = new HashMap<>();
 		String param;
 		boolean receiveParam = false;
 		Gson gson = new Gson();
 
-		String[] knownParams = {"category", "keywords", "openedBids", "currentBids", "wonBids", "myCurrentSales", "myPendingSales", "myEndedSales", "searchType"};
-
-		// No item is currently selected. This is necessary for the image handler.
-		request.getSession().removeAttribute("item");
-		request.getSession().removeAttribute("itemImages");
-
-		try
-		{
-			// Read the available categories.
-			request.setAttribute("categories", managerFactory.getCategoryManager().find(null));
-		}
-		catch (ModelException e)
-		{
-			// Do nothing.
-		}
+		// Connected user.
+		User user = (User) request.getSession().getAttribute("user");
 
 		// See if known parameters are received from the request.
 		for (String knownParam : knownParams)
@@ -89,40 +178,33 @@ public class WelcomeServlet extends HttpServlet
 				}
 			}
 
-			// If nothing is defined, put the default values.
-			if (savedParams.size() == 0)
+			// If nothing is defined, put the default values. Do the same if the user
+			// is not connected because in that case, he can only see opened bids.
+			if ( (savedParams.size() == 0) || (user == null) )
 			{
 				// The user will search for the purchases.
-				savedParams.put("searchType", "purchases");
+				savedParams.put(SEARCH_TYPE, "purchases");
 
 				// and available items to buy.
-				savedParams.put("openedBids", "openedBids");
+				savedParams.put(OPENED_BIDS, "1");
 			}
 		}
 
-		try
+		if (user == null)
 		{
-			//request.setAttribute("items", ItemManager.getInstance().findByCriteria(null, UserRole.BUYER, new Search().setOpenedBids(), null, ""));
-			request.setAttribute("items", managerFactory.getItemManager().find(null));
-		}
-		catch (ModelException e)
-		{
-			request.setAttribute("items", new ArrayList<Item>());
+			savedParams.remove(MY_CURRENT_SALES);
+			savedParams.remove(MY_ENDED_SALES);
+			savedParams.remove(MY_PENDING_SALES);
 		}
 
 		// Puts received parameter to request to be accessed by the JSP.
 		savedParams.forEach(request::setAttribute);
 
+		System.out.println("filters parameters:");
+		for (String s : knownParams) System.out.printf("%s = %s\n", s, request.getAttribute(s));
+		System.out.println();
+
 		// Send to the browser the updated cookie.
 		response.addCookie(new Cookie("searchOptions2", Base64.getEncoder().encodeToString(gson.toJson(savedParams).getBytes())));
-
-		// Go to the welcome page => take search options into account.
-		RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/welcome.jsp");
-		requestDispatcher.forward(request, response);
-	}
-
-	@Override protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-	{
-		doGet(request, response);
 	}
 }
